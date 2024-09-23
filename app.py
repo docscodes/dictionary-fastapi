f1e7c3da3693800a680ad1b75d647a1c658d6a5e
docscodes <docscodes@gmail.com>
2024-09-23 19:49:44 -0400

    fix indent

--- app.py
+++ app.py
@@ -8,47 +8,50 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    response = {"usage": "/dict?=<word>"}
-    return jsonable_encoder(response)
+  response = {"usage": "/dict?=<word>"}
+  return jsonable_encoder(response)
 
 
 @app.get("/dict")
 def dictionary(word: str):
-    if not word:
-        response = {"status": "error", "word": word, "data": "word not found"}
-        return jsonable_encoder(response)
+  if not word:
+    response = {"status": "error", "word": word, "data": "word not found"}
+    return jsonable_encoder(response)
 
-    definitions = match_exact(word)
-    if definitions:
-        response = {"status": "success", "word": word, "data": definitions}
-        return jsonable_encoder(response)
+  definitions = match_exact(word)
+  if definitions:
+    response = {"status": "success", "word": word, "data": definitions}
+    return jsonable_encoder(response)
 
-    definitions = match_like(word)
-    if definitions:
-        response = {"status": "partial", "word": word, "data": definitions}
-        return jsonable_encoder(response)
-    else:
-        response = {"status": "error", "word": word, "data": "word not found"}
-        return jsonable_encoder(response)
+  definitions = match_like(word)
+  if definitions:
+    response = {"status": "partial", "word": word, "data": definitions}
+    return jsonable_encoder(response)
+  else:
+    response = {"status": "error", "word": word, "data": "word not found"}
+    return jsonable_encoder(response)
 
 
 @app.get("/dict_list")
 def dictionary_list(words: List[str] = Query(None)):
-    if not words:
-        response = {"status": "error", "word": words, "data": "word not found"}
-        return jsonable_encoder(response)
-
-    response = {"words": []}
-
-    for word in words:
-        definitions = match_exact(word)
-        if definitions:
-            response["words"].append({"status": "success", "word": word, "data": definitions})
-        else:
-            definitions = match_like(word)
-            if definitions:
-                response["words"].append({"status": "partial", "word": word, "data": definitions})
-            else:
-                response[words].append({"status": "error", "word": word, "data": "word not found"})
-
+  if not words:
+    response = {"status": "error", "word": words, "data": "word not found"}
     return jsonable_encoder(response)
+
+  response = {"words": []}
+
+  for word in words:
+    definitions = match_exact(word)
+    if definitions:
+      response["words"].append(
+          {"status": "success", "word": word, "data": definitions})
+    else:
+      definitions = match_like(word)
+      if definitions:
+        response["words"].append(
+            {"status": "partial", "word": word, "data": definitions})
+      else:
+        response[words].append(
+            {"status": "error", "word": word, "data": "word not found"})
+
+  return jsonable_encoder(response)
--- model/dbHandler.py
+++ model/dbHandler.py
@@ -2,20 +2,20 @@ import sqlite3 as SQL
 
 
 def match_exact(word: str) -> list:
-    db = SQL.connect("data/dictionary.db")
-    sql_query = "SELECT * from entries WHERE word=?"
+  db = SQL.connect("data/dictionary.db")
+  sql_query = "SELECT * from entries WHERE word=?"
 
-    match = db.execute(sql_query, (word,)).fetchall()
-    db.close()
+  match = db.execute(sql_query, (word,)).fetchall()
+  db.close()
 
-    return match
+  return match
 
 
 def match_like(word: str) -> list:
-    db = SQL.connect("data/dictionary.db")
-    sql_query = "SELECT * from entries WHERE word LIKE ?"
+  db = SQL.connect("data/dictionary.db")
+  sql_query = "SELECT * from entries WHERE word LIKE ?"
 
-    match = db.execute(sql_query, ("%" + word + "%",)).fetchall()
-    db.close()
-    
-    return match
+  match = db.execute(sql_query, ("%" + word + "%",)).fetchall()
+  db.close()
+
+  return match
